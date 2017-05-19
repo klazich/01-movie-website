47f916bc3bf2f4204ea874efeb48f816c0e29db8
Kevin <kevin.lazich@gmail.com>
2017-05-19 00:35:29 -0400

    Not a commit
    this is a push to work.
    
    TODO: amend/cleanup commit tomorrow

--- media.py
+++ media.py
@@ -10,34 +10,63 @@
 # response = requests.get(url)
 # python_dictionary_values = json.loads(response.text)
 
-# The Movie Database API
-# api_key: "4ba86a24d84ecbf254b1cee306e0f9ed"
-# example:
-#     https://api.themoviedb.org/3/movie/550?api_key=4ba86a24d84ecbf254b1cee306e0f9ed
-
 # goo.gl api
 # api_key: "AIzaSyBnC_nAB4UbOJCvhZ52rX8X92N1V1Tgeto"
 
 # youtube api
 # api_key: "AIzaSyBlr3kG98VwGz5D3QufXG2dqXgj6HDnwpQ"
 
-# Google API Pyhton Client
-# run: pip install --upgrade google-api-python-client
 
-import webbrowser
-import json
-import requests
+from webbrowser import open
+from json import loads
+from requests import get
+
+OMDB_API_KEY = 'df397f1b'
+OMDB_QUERY = 'http://www.omdbapi.com/?apikey={}'.format(OMDB_API_KEY)
 
 
-class Movie():
-    def __init__(self, movie_title, movie_storyline=None, poster_image=None, trailer_youtube=None):
+def get_movie_imdb_id(movie, year=None):
+    request = ''.join([OMDB_QUERY, '&s=', movie.replace(' ', '+')])
+    if year is not None:
+        request = "".join([request, '&y=', year])
+
+    response = get(request)
+
+    response_dictionary = loads(response.text)
+
+    if response_dictionary['Response'] == 'True':
+        imdb_id = response_dictionary['Search'][0]['imdbID']
+        return imdb_id
+    else:
+        return None
+
+
+class Movie:
+    """
+    
+    """
+
+    def __init__(self, movie_title, trailer_youtube, release_year=None):
+        """
+
+        Args:
+            movie_title (str): The title of the movie
+            trailer_youtube (str): A youtube URL to a trailer of the movie 
+            release_year (:obj:`str`, optional): 
+        """
         self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+        # self.storyline = movie_storyline
+        # self.poster_image_url = poster_image
 
-    def __fetch_movie_data(self):
-        query_string = 'http://www.omdbapi.com/?t=%s' % (self.movie_title)
+    def show_trailer(self):
+        open(self.trailer_youtube_url)
+
+    def _fetch_movie_data(self):
+        # a request for movie mad max, year 2015
+        # http://www.omdbapi.com/?t=mad+max&y=2015
+        url_query = 'http://www.omdbapi.com/?t={}'.format(self.title)
+        response = get(url_query)
+        res_dict = loads(response.text)
+        return res_dict
