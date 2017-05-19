562e808e267f46f30ef2bcac1c2279b9ab4a0059
Kevin <kevin.lazich@gmail.com>
2017-05-19 16:55:15 -0400

    Add OMDB api integration for movie meta data
    This commit adds the get_movie_meta method to Movie class. It uses the
    requests Python library to make a GET request to omdbapi.com. the request
    uses the movie title and the movie release year if a year is passed to
    Movie construction.
    
    .raise_for_status() raises an exception if status codes:
    	4xx
    	5xx

--- media.py
+++ media.py
@@ -1,7 +1,11 @@
 """
 
 """
-from webbrowser import open as open_url
+import webbrowser
+import requests
+
+OMDB_API_KEY = 'df397f1b'
+OMDB_URL = 'http://www.omdbapi.com/'
 
 
 class Movie:
@@ -18,4 +22,14 @@ class Movie:
         # self.poster_image_url = poster_image
 
     def show_trailer(self):
-        open_url(self.trailer_youtube_url)
+        webbrowser.open(self.trailer_youtube_url)
+
+    def get_movie_meta(self):
+        payload = {'apikey': OMDB_API_KEY,
+                   't': self.title,
+                   'y': self.release_year}
+        response = requests.get(OMDB_URL, params=payload)
+
+        response.raise_for_status()
+
+        return response.json()
