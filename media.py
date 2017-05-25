6a62fe0d4a2f9ef7c67dba6d3f66d2224f117eb1
Kevin <kevin.lazich@gmail.com>
2017-05-25 01:16:10 -0400

    Moves API calls to single static method.
    The API intigrations in search.py are used in static method
    `get_movie_metadata` that returns a dictionary of movie data compiled from
    youtube and omdb searches.

--- media.py
+++ media.py
@@ -1,35 +1,74 @@
-"""
-
-"""
 import webbrowser
-import requests
+import search
+import config
 
-OMDB_API_KEY = 'df397f1b'
-OMDB_URL = 'http://www.omdbapi.com/'
+# For constructing a youtube link from a youtube video ID.
+YOUTUBE_URL = 'https://www.youtube.com/watch?v={}'
+
+get_youtube_id = search.youtube_search(config.YOUTUBE_API_KEY)
+get_omdb_movie_info = search.omdb_search(config.OMDB_API_KEY)
 
 
 class Movie:
+    """A class for encapsulating a movie's information
+    
+    Creates Movie entities that are used by fresh_tomatoes.py in rendering 
+    the website's HTML. 
     """
 
-    """
-
-    def __init__(self, movie_title, trailer_youtube, release_year=None):
-        self.title = movie_title
-        self.trailer_youtube_url = trailer_youtube
-        self.release_year = release_year
-
-        # self.storyline = movie_storyline
-        # self.poster_image_url = poster_image
+    def __init__(self, title, storyline, youtube_url, image_url,
+                 release_year=None, movie_info=None):
+        """Constructor for Movie class.
+        
+        Sets Movie instance attributes.
+        
+        Args:
+            title (str): The title of the movie.
+            storyline (str): The plot/storyline of the movie.
+            youtube_url (str): A URL pointing to a                 
+                youtube.com video of a trailer of the movie.
+            image_url (str): A URL pointing to an image of the movie 
+                poster or box art.
+            release_year (str|None): The year the move was 
+                released in theater.
+            movie_info(dict|None): An optional dictionary of 
+                additional movie information.
+        """
+        self.title = title
+        self.year = release_year
+        self.storyline = storyline
+        self.trailer_youtube_url = youtube_url
+        self.poster_image_url = image_url
+        self.info = movie_info
 
     def show_trailer(self):
+        """Opens trailer_youtube_url in default web browser."""
         webbrowser.open(self.trailer_youtube_url)
 
-    def get_movie_meta(self):
-        payload = {'apikey': OMDB_API_KEY,
-                   't': self.title,
-                   'y': self.release_year}
-        response = requests.get(OMDB_URL, params=payload)
+    def show_poster(self):
+        """Opens the poster_image_url in default web browser."""
+        webbrowser.open(self.poster_image_url)
 
-        response.raise_for_status()
+    @staticmethod
+    def get_movie_metadata(movie_title, year=None, *queries):
+        """Fetch data on movie using omdbapi and youtube API.
 
-        return response.json()
+        Using the functions from search.py this static function returns a 
+
+        Args:
+            movie_title (str): The movie title.
+            year (str|None): The movie's release year.
+            *queries (str): Additional query terms.
+
+        Returns:
+            dict: A dictionary of compiled movie info.
+        """
+        movie_info = get_omdb_movie_info(movie_title, year)
+        video_id = get_youtube_id(movie_title, year, *queries)
+
+        metadata = movie_info
+        # Add youtube data to dictionary
+        metadata['trailer_youtube_id'] = video_id
+        metadata['trailer_youtube_url'] = YOUTUBE_URL.format(video_id)
+
+        return metadata
